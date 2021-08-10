f81cc841de361f4cd844837376764fa069547f80
kamal khatri <59205492+ZERO-KAMAL@users.noreply.github.com>
2021-08-10 14:02:11 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+ 
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        navigation:false,
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});
